41f0ddcd73fb2987a5a6e08cf5e709bf79a4d5d4
rishigarg09 <rishigarg185@gmail.com>
2024-08-02 20:29:59 +0530

    added dependencies for project init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  css: ['@/assets/css/tailwind.css'],
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    '@vueuse/nuxt'
+  ],
   devtools: { enabled: true }
 })
